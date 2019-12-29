982554b44c224703525453be33d5fdfadf11e79e
digitalpartisan <digital.partisan.86@gmail.com>
2019-12-28 17:11:03 -0800

    Updated scaffold config

--- .scaffold/config.sh
+++ .scaffold/config.sh
@@ -1,4 +1,2 @@
-SCAFFOLD_MOD_NAME="PowerArmorEnhancements"
-SCAFFOLD_MOD_GAME_PATH="/k/Steam/steamapps/common/Fallout 4"
-SCAFFOLD_MOD_DELIVERABLES_PATH="/k/FO4 Modding/Packaging"
-SCAFFOLD_MOD_DEPENDENCIES="Jiffy,InjectTec,Chronicle"
+SCAFFOLD_MOD="PowerArmorEnhancements"
+SCAFFOLD_GAME="fallout4"
